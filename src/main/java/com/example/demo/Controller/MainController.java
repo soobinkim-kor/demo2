00efea70e389762b4f0439422f15481350a39916
soobinkim-kor <shb03207@naver.com>
2024-03-16 14:24:42 +0900

    2nd Commit
    Controller 클래스 생성
    Bootstrap 테마 추가

---
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String controllerTest(){
+        return "/hello.html";
+    }
+}
