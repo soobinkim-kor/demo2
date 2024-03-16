f8e97704fb4df8a511d5a8cb9fa61a944a79aa22
soobinkim-kor <shb03207@naver.com>
2024-03-16 16:03:15 +0900

    3rd Commit

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -1,13 +1,24 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.User;
+import com.example.demo.Service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class MainController {
 
-    @GetMapping(value = "/")
-    public String controllerTest(){
-        return "/hello.html";
+    private final UserService userService;
+    @GetMapping(value = "/test")
+    public ResponseEntity<String> controllerTest(@RequestBody User userParam){
+        User user = User.builder().userName("Soobin").build();
+        userService.saveUser(user);
+        return new ResponseEntity<>("hello", HttpStatus.OK);
     }
 }
