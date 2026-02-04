b43c412e25ca78958ba218debf672c6e02a32316
soobinkim <shb03207@naver.com>
2026-02-04 23:05:22 +0900

    로깅 aop kafka

---
+++ src/main/java/com/example/demo/kafka/producer/LogEventProducer.java
@@ -0,0 +1,19 @@
+package com.example.demo.kafka.producer;
+
+import com.example.demo.kafka.model.LogEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogEventProducer {
+
+    private static final String TOPIC = "log-event";
+
+    private final KafkaTemplate<String, LogEvent> kafkaTemplate;
+
+    public void send(LogEvent event) {
+        kafkaTemplate.send(TOPIC, event);
+    }
+}
