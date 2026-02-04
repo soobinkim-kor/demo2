9fa2fec45391ed68406fab69b486547b103a7546
soobinkim <shb03207@naver.com>
2026-02-04 23:05:32 +0900

    로깅 aop kafka

--- src/main/java/com/example/demo/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/demo/kafka/config/KafkaConsumerConfig.java
@@ -1,4 +1,48 @@
 package com.example.demo.kafka.config;
 
+import com.example.demo.kafka.model.LogEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.*;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, LogEvent> logConsumerFactory() {
+
+        JsonDeserializer<LogEvent> deserializer =
+                new JsonDeserializer<>(LogEvent.class);
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "log-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, LogEvent>
+    logKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, LogEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(logConsumerFactory());
+        return factory;
+    }
 }
--- src/main/java/com/example/demo/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/example/demo/kafka/config/KafkaProducerConfig.java
@@ -1,4 +1,32 @@
 package com.example.demo.kafka.config;
 
+import com.example.demo.kafka.model.LogEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, LogEvent> logProducerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, LogEvent> logKafkaTemplate() {
+        return new KafkaTemplate<>(logProducerFactory());
+    }
 }
