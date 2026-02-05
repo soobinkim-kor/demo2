98354fe11e6e5e9d592de65ebcfd041272afe558
soobinkim <shb03207@naver.com>
2026-02-05 22:11:19 +0900

    로깅 aop kafka

--- src/main/java/com/example/demo/global/aspect/logging/listener/LogEventListener.java
+++ src/main/java/com/example/demo/global/aspect/logging/listener/LogEventListener.java
@@ -1,7 +1,7 @@
 package com.example.demo.global.aspect.logging.listener;
 
 import com.example.demo.global.aspect.logging.event.LogEvent;
-import com.example.demo.global.aspect.logging.service.LoggingService;
+import com.example.demo.global.aspect.logging.service.AspectLoggingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class LogEventListener {
 
-    private final LoggingService loggingService;
+    private final AspectLoggingService aspectLoggingService;
 
     @Async("loggingExecutor")
     @EventListener
     public void handle(LogEvent event) {
-        loggingService.save(event.getLogData());
+        aspectLoggingService.save(event.getLogData());
     }
 }
--- src/main/java/com/example/demo/global/aspect/logging/service/AspectLoggingService.java
+++ src/main/java/com/example/demo/global/aspect/logging/service/AspectLoggingService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class LoggingService {
+public class AspectLoggingService {
 
     private final LoggingEventRepository repository;
 
