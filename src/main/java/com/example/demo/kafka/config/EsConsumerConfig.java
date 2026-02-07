6de1c59ab55a6587bdd09c52268538727d54dd9f
soobinkim <shb03207@naver.com>
2026-02-07 17:41:48 +0900

    kafka abstract consumer

--- src/main/java/com/example/demo/kafka/config/EsConsumerConfig.java
+++ src/main/java/com/example/demo/kafka/config/EsConsumerConfig.java
@@ -2,11 +2,17 @@ package com.example.demo.kafka.config;
 
 import com.example.demo.global.aspect.logging.event.LogEvent;
 import com.example.demo.kafka.core.config.AbstractKafkaConsumerConfig;
+
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class EsConsumerConfig extends AbstractKafkaConsumerConfig<LogEvent> {
@@ -17,7 +23,9 @@ public class EsConsumerConfig extends AbstractKafkaConsumerConfig<LogEvent> {
 
     @Bean(name = "logConsumerFactory")
     public ConsumerFactory<String, LogEvent> logConsumerFactory() {
-        return createConsumerFactory(LogEvent.class);
+        Map<String, Object> additionalProps =  new HashMap<>();
+
+        return createConsumerFactory(LogEvent.class, additionalProps);
     }
 
     @Bean(name = "logKafkaListenerContainerFactory")
