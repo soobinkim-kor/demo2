f5d84301e7fd133679f1d6bdd46f361e384680a2
soobinkim <shb03207@naver.com>
2026-02-07 16:13:10 +0900

    elasticsearch 설정 기존 logconsumer 와 esconsumer 를 명시적으로 분리

--- src/main/java/com/example/demo/kafka/producer/LogEventProducer.java
+++ src/main/java/com/example/demo/kafka/producer/LogEventProducer.java
@@ -2,18 +2,22 @@ package com.example.demo.kafka.producer;
 
 import com.example.demo.global.aspect.logging.event.LogEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class LogEventProducer {
-
     private static final String TOPIC = "log-event";
 
+    @Qualifier("logMessageKafkaTemplate")
     private final KafkaTemplate<String, LogEvent> kafkaTemplate;
 
     public void send(LogEvent event) {
         kafkaTemplate.send(TOPIC, event);
+    }
+
+    public LogEventProducer(KafkaTemplate<String, LogEvent> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
     }
 }
